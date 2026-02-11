7743a6c2f9dcc515f288acfb2ff620d5df2d5559
chaker <fanen000@gmail.com>
2026-02-11 18:28:19 +0100

    mainadmin update (not functional waiting for getcategory String) and some touches

--- src/main/java/org/openjfx/sfaxbest/AdminController.java
+++ src/main/java/org/openjfx/sfaxbest/AdminController.java
@@ -1,13 +1,14 @@
 package org.openjfx.sfaxbest;
 
 import Services.FilmService;
+import entities.Category;
 import entities.Film;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.time.LocalDate;
@@ -23,6 +24,20 @@ public class AdminController {
     @FXML private TableColumn<Film, String> colBanner;
     @FXML private TableColumn<Film, Integer> colDuration;
     @FXML private TableColumn<Film, String> colVideo;
+    @FXML private TableColumn<Film, String> colPoster;
+    @FXML private TableColumn<Film, List<Category>> colCategorie;
+    @FXML private TextField tfTitle;
+    @FXML private TextArea taDescription;
+    @FXML private TextField tfReleaseYear;
+    @FXML private TextField tfDuration;
+    @FXML private TextField tfTrailer;
+    @FXML private TextField tfBanner;
+    @FXML private TextField tfVideo;
+    @FXML private VBox form;
+    @FXML private Button add2btn;
+    @FXML private Button edit2btn;
+
+
     FilmService filmService= new FilmService();
     @FXML
     public void goToFilms() throws IOException {
@@ -38,6 +53,24 @@ public class AdminController {
     }
     @FXML
     public void initialize() {
+        form.setManaged(false);
+        add2btn.setVisible(false);
+        edit2btn.setVisible(false);
+        add2btn.setManaged(false);
+        edit2btn.setManaged(false);
+        colId.prefWidthProperty().bind(movieTable.widthProperty().multiply(0.06)); // 6%
+        colTitle.prefWidthProperty().bind(movieTable.widthProperty().multiply(0.18)); // 18%
+        colDescription.prefWidthProperty().bind(movieTable.widthProperty().multiply(0.30)); // 30%
+        colReleaseDate.prefWidthProperty().bind(movieTable.widthProperty().multiply(0.08));
+        colTrailer.prefWidthProperty().bind(movieTable.widthProperty().multiply(0.12));
+        colBanner.prefWidthProperty().bind(movieTable.widthProperty().multiply(0.12));
+        colDuration.prefWidthProperty().bind(movieTable.widthProperty().multiply(0.07));
+        colVideo.prefWidthProperty().bind(movieTable.widthProperty().multiply(0.07));
+
+        loadFilms();
+    }
+
+    public void loadFilms() {
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
         colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
         colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
@@ -46,8 +79,12 @@ public class AdminController {
         colBanner.setCellValueFactory(new PropertyValueFactory<>("pathBanner"));
         colDuration.setCellValueFactory(new PropertyValueFactory<>("durationSeconds"));
         colVideo.setCellValueFactory(new PropertyValueFactory<>("pathVideo"));
+        //colPoster.setCellValueFactory(new PropertyValueFactory<>("pathPoster"));
+        //colCategorie.setCellValueFactory(new PropertyValueFactory<>("Category"));
 
-        movieTable.setItems(FXCollections.observableArrayList(new Film(
+        movieTable.setItems(FXCollections.observableArrayList(FXCollections.observableArrayList(
+
+                new Film(
                         "Inception",
                         "A thief who steals corporate secrets through dream-sharing technology.",
                         2010,
@@ -90,7 +127,82 @@ public class AdminController {
                         "videos/avatar.mp4",
                         "poster/avatar.jpg"
                 )
-        ));
+        )));
+    }
+    @FXML
+    public void editFilms() {
+        form.setManaged(true);
+        form.setVisible(true);
+        edit2btn.setVisible(true);
+        edit2btn.setManaged(true);
+        add2btn.setVisible(false);
+        add2btn.setManaged(false);
+
+        movieTable.setOnMouseClicked(event->{
+            Film newSelection = movieTable.getSelectionModel().getSelectedItem();
+            if(newSelection!=null){
+                String id = String.valueOf(newSelection.getId());
+                String title = newSelection.getTitle();
+                String description = newSelection.getDescription();
+                String releaseDate = String.valueOf(newSelection.getReleaseYear());
+                String trailer = newSelection.getPathTrailer();
+                String banner = newSelection.getPathBanner();
+                String duration = String.valueOf(newSelection.getDurationSeconds());
+                String video = newSelection.getPathVideo();
+                tfTitle.setText(title);
+                tfBanner.setText(banner);
+                tfTrailer.setText(duration);
+                tfTrailer.setText(trailer);
+                tfBanner.setText(banner);
+                taDescription.setText(description);
+                tfVideo.setText(video);
+                tfReleaseYear.setText(releaseDate);
+                tfDuration.setText(duration);
+                //ne9sin el poster & category
+
+            }
+        });
+        loadFilms();
+
+
+    }
+    @FXML
+    public void addFilms() {
+        movieTable.setOnMouseClicked(event->{});
+        tfTitle.setText(null);
+        tfBanner.setText(null);
+        tfTrailer.setText(null);
+        tfDuration.setText(null);
+        tfVideo.setText(null);
+        tfReleaseYear.setText(null);
+        taDescription.setText(null);
+
+        form.setManaged(true);
+        form.setVisible(true);
+        add2btn.setVisible(true);
+        add2btn.setManaged(true);
+        edit2btn.setVisible(false);
+        edit2btn.setManaged(false);
+        /*String title = tfTitle.getText();
+        String description = taDescription.getText();
+        int releaseDate = Integer.parseInt(tfReleaseYear.getText());
+        String trailer = tfTrailer.getText();
+        String banner = tfBanner.getText();
+        int duration = Integer.parseInt(tfDuration.getText());
+        String video = tfVideo.getText();*/
+        //filmService.save(title,description,trailer,banner,releaseDate,duration,video,1,2);
+        loadFilms();
+    }
+    public void deleteFilms() {
+        movieTable.setOnMouseClicked(event->{
+            Film newSelection = movieTable.getSelectionModel().getSelectedItem();
+            if(newSelection!=null){
+                Long id = newSelection.getId();
+                filmService.delete(id);
+                loadFilms();
+            }
+        });
+
     }
 
 
