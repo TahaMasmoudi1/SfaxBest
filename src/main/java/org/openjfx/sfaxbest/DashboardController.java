89457714033cddbb0f580fb885eb644634b9579b
MED CHAKER <fanen000@gmail.com>
2026-03-15 15:05:25 +0100

    Documantary dashboards charts waiting for data ...

---
+++ src/main/java/org/openjfx/sfaxbest/DashboardController.java
@@ -0,0 +1,82 @@
+package org.openjfx.sfaxbest;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+
+import java.io.IOException;
+
+public class DashboardController {
+
+
+    @FXML
+    private PieChart categoryPieChart;
+
+    @FXML
+    private BarChart<String, Number> topMoviesBarChart;
+
+    @FXML
+    private LineChart<String, Number> subscribersLineChart;
+
+    @FXML
+    public void goToSeries() throws IOException {
+        App.setRoot("seriesAdmin");
+    }
+    @FXML
+    public void goToDocumantary() throws IOException {
+        App.setRoot("documantaryAdmin");
+    }
+    @FXML
+    public void goToFilms() throws IOException {
+        App.setRoot("mainAdmin");
+    }
+    @FXML
+    public void initialize() {
+
+        loadCategoryChart();
+        loadTopMoviesChart();
+        loadSubscribersChart();
+
+    }
+    private void loadCategoryChart() {
+
+        ObservableList<PieChart.Data> data =
+                FXCollections.observableArrayList(
+                        new PieChart.Data("Horror", 36),
+                        new PieChart.Data("Comedy", 14),
+                        new PieChart.Data("Drama", 80)
+                );
+
+        categoryPieChart.setData(data);
+    }
+    private void loadTopMoviesChart() {
+
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("Top Movies");
+
+        series.getData().add(new XYChart.Data<>("Batman", 2000));
+        series.getData().add(new XYChart.Data<>("Matrix", 1800));
+        series.getData().add(new XYChart.Data<>("Avatar", 1700));
+        series.getData().add(new XYChart.Data<>("Inception", 1600));
+        series.getData().add(new XYChart.Data<>("Titanic", 1500));
+
+        topMoviesBarChart.getData().add(series);
+    }
+    private void loadSubscribersChart() {
+
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+        series.setName("New Subscribers");
+
+        series.getData().add(new XYChart.Data<>("Mon", 20));
+        series.getData().add(new XYChart.Data<>("Tue", 35));
+        series.getData().add(new XYChart.Data<>("Wed", 15));
+        series.getData().add(new XYChart.Data<>("Thu", 40));
+        series.getData().add(new XYChart.Data<>("Fri", 28));
+
+        subscribersLineChart.getData().add(series);
+    }
+}
--- src/main/java/org/openjfx/sfaxbest/DocumantaryController.java
+++ src/main/java/org/openjfx/sfaxbest/DocumantaryController.java
@@ -1,20 +1,18 @@
 package org.openjfx.sfaxbest;
 
-import Services.FilmService;
+import Services.DocumentaryService;
 import entities.Category;
 import entities.Film;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.List;
 
-public class AdminController {
+public class DocumantaryController {
     @FXML private TableView<Film> movieTable;
     @FXML private TableColumn<Film, Integer> colId;
     @FXML private TableColumn<Film, String> colTitle;
@@ -38,18 +36,14 @@ public class AdminController {
     @FXML private Button edit2btn;
 
 
-    FilmService filmService= new FilmService();
+    DocumentaryService documentaryService = new DocumentaryService();
+    @FXML
+    public void goToFilms() throws IOException {
+        App.setRoot("mainAdmin");
+    }
     @FXML
     public void goToSeries() throws IOException {
         App.setRoot("seriesAdmin");
-    }
-    @FXML
-    public void goToDocumantary() throws IOException {
-        App.setRoot("documantaryAdmin");
-    }
-    @FXML
-    public void goToDashbaord() throws IOException{
-        App.setRoot("dashboardAdmin");
     }
     @FXML
     public void initialize() {
@@ -67,10 +61,10 @@ public class AdminController {
         colDuration.prefWidthProperty().bind(movieTable.widthProperty().multiply(0.07));
         colVideo.prefWidthProperty().bind(movieTable.widthProperty().multiply(0.07));
 
-        loadFilms();
+        loadDocumantary();
     }
 
-    public void loadFilms() {
+    public void loadDocumantary() {
         colId.setCellValueFactory(new PropertyValueFactory<>("id"));
         colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
         colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
@@ -108,7 +102,7 @@ public class AdminController {
 
                 new Film(
                         "The Matrix",
-                        "A hacker discovers reality is a simulation.",
+                        "wiiiiiw",
                         1999,
                         "trailer/matrix.mp4",
                         "banner/matrix.jpg",
@@ -130,7 +124,7 @@ public class AdminController {
         )));
     }
     @FXML
-    public void editFilms() {
+    public void editDocumantary() {
         form.setManaged(true);
         form.setVisible(true);
         edit2btn.setVisible(true);
@@ -162,12 +156,12 @@ public class AdminController {
 
             }
         });
-        loadFilms();
+        loadDocumantary();
 
 
     }
     @FXML
-    public void addFilms() {
+    public void addDocumantary() {
         movieTable.setOnMouseClicked(event->{});
         tfTitle.setText(null);
         tfBanner.setText(null);
@@ -191,15 +185,15 @@ public class AdminController {
         int duration = Integer.parseInt(tfDuration.getText());
         String video = tfVideo.getText();*/
         //filmService.save(title,description,trailer,banner,releaseDate,duration,video,1,2);
-        loadFilms();
+        loadDocumantary();
     }
-    public void deleteFilms() {
+    public void deleteDocumantary() {
         movieTable.setOnMouseClicked(event->{
             Film newSelection = movieTable.getSelectionModel().getSelectedItem();
             if(newSelection!=null){
                 Long id = newSelection.getId();
-                filmService.delete(id);
-                loadFilms();
+                documentaryService.delete(id);
+                loadDocumantary();
             }
         });
 
