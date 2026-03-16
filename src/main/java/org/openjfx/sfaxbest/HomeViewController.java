7cbae6fc25549dbfc9113acbec9ce720c176b68d
jo bangalo <jawhermonastiri10@gmail.com>
2026-03-16 12:09:52 +0100

    Home view 0.2

--- src/main/java/org/openjfx/sfaxbest/HomeViewController.java
+++ src/main/java/org/openjfx/sfaxbest/HomeViewController.java
@@ -1,18 +1,21 @@
 package org.openjfx.sfaxbest;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.Interpolator;
-import javafx.animation.TranslateTransition;
+import javafx.animation.*;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
+import org.openjfx.sfaxbest.MoviePosterController;
 
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,12 +26,6 @@ public class HomeViewController {
     @FXML private Pane SideMenuPane;
     private boolean menuVisible = false;
 
-
-    @FXML
-    private void InitializeMenu() {
-        toggleMenu();
-    }
-
     @FXML
     private void toggleMenu() {
         TranslateTransition transition = new TranslateTransition(Duration.millis(250), VBsideMenu);
@@ -36,7 +33,7 @@ public class HomeViewController {
 
         if (menuVisible) {
             // Slide OUT
-            transition.setToX(-VBsideMenu.getPrefWidth());
+            transition.setToX(-VBsideMenu.getPrefWidth() - 20);
             VBsideMenu.setMouseTransparent(true);
             // Backwards fade
             fadeTransition.setFromValue(0.7);
@@ -62,56 +59,112 @@ public class HomeViewController {
         menuVisible = !menuVisible;
         transition.play();
     }
-    //Hero logic
-    @FXML private HBox heroRow;
-    @FXML private ImageView prevHero;
-    @FXML private ImageView activeHero;
-    @FXML private ImageView nextHero;
-    @FXML private Label heroTitle;
 
-    private final List<Image> heroImages = new ArrayList<>();
+    //Hero logic
+
+
+    @FXML private ImageView activeHero;
+
+    @FXML private StackPane activeSlot;
+
+    private List<Image> heroImages = new ArrayList<>();
+
+    private List<Image> popularPosters = new ArrayList<>();
+    private List<Image> trendingPosters = new ArrayList<>();
+
+    @FXML private HBox PopularPosterRow;
+    @FXML private HBox TrendingPosterRow;
     private int currentIndex = 0;
+    private Timeline slider;
+
 
     @FXML
     public void initialize() {
-        heroImages.add(new Image(getClass().getResource("/Images/blackpanther_hero.jpg").toExternalForm()));
-        heroImages.add(new Image(getClass().getResource("/Images/galacticwar_panel.png").toExternalForm()));
-        heroImages.add(new Image(getClass().getResource("/Images/Gladiator_hero.jpg").toExternalForm()));
+        String imagePath1 = getClass().getResource("/Images/Gladiator_hero.jpg").toExternalForm();
+        String imagePath2 = getClass().getResource("/Images/galacticwar_panel.png").toExternalForm();
+        String imagePath3 = getClass().getResource("/Images/Venom_hero.jpg").toExternalForm();
 
-        updateHeroes();
-    }
-    private void updateHeroes() {
-        int prevIndex = (currentIndex - 1 + heroImages.size()) % heroImages.size();
-        int nextIndex = (currentIndex + 1) % heroImages.size();
+        String posterPath1 = getClass().getResource("/Images/the_matrix_poster.jpg").toExternalForm();
+        String posterPath2 = getClass().getResource("/Images/gladiator_poster.jpg").toExternalForm();
+        String posterPath3 = getClass().getResource("/Images/dark_Knight_poster.jpg").toExternalForm();
+        heroImages = List.of(
+                new Image(imagePath1),
+                new Image(imagePath2),
+                new Image(imagePath3)
+        );
+        popularPosters = List.of(
+                new Image(posterPath1),
+                new Image(posterPath2),
+                new Image(posterPath3)
+        );
+        //For testing
+        trendingPosters = popularPosters;
 
-        prevHero.setImage(heroImages.get(prevIndex));
+        loadPopularRow();
+        loadTrendingRow();
+
         activeHero.setImage(heroImages.get(currentIndex));
-        nextHero.setImage(heroImages.get(nextIndex));
+        slider = new Timeline(new KeyFrame(Duration.seconds(4), e -> nextBanner()));
 
-        heroTitle.setText("FEATURED " + (currentIndex + 1));
+        slider.setCycleCount(Animation.INDEFINITE);
+        slider.play();
+        activeSlot.setOnMouseEntered(e -> slider.pause());
+        activeSlot.setOnMouseExited(e -> slider.play());
+
     }
+
     @FXML
-    public void previousHero() {
-        slideCarousel(1);
-    }
-    @FXML
-    public void nextHero() {
-        slideCarousel(-1);
-    }
-    private void slideCarousel(int direction) {
-        double slideDistance = 200; // roughly width of one banner + spacing
+    public void nextBanner() {
+        FadeTransition fadeOut = new FadeTransition(Duration.millis(400), activeHero);
+        fadeOut.setToValue(0);
 
-        TranslateTransition slide = new TranslateTransition(Duration.millis(350), heroRow);
-        slide.setByX(direction * slideDistance);
-        slide.setInterpolator(Interpolator.EASE_BOTH);
+        fadeOut.setOnFinished(e -> {
 
-        slide.setOnFinished(e -> {
-            currentIndex = (currentIndex - direction + heroImages.size()) % heroImages.size();
-            updateHeroes();
-            heroRow.setTranslateX(0);
+            currentIndex = (currentIndex + 1) % heroImages.size();
+            activeHero.setImage(heroImages.get(currentIndex));
+
+            FadeTransition fadeIn = new FadeTransition(Duration.millis(400),activeHero);
+            fadeIn.setToValue(1);
+            fadeIn.play();
         });
 
-        slide.play();
+        fadeOut.play();
     }
-}
+    private void loadPopularRow() {
+        try{
+            for (Image image : popularPosters){
 
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("movie-poster-card.fxml"));
+                Node cardNode = loader.load();
+
+                MoviePosterController cardController = loader.getController();
+
+                cardController.setData(image);
+
+                PopularPosterRow.getChildren().add(cardNode);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't load Popular posters");
+        }
+    }
+    private void loadTrendingRow() {
+        try{
+            for (Image image : trendingPosters){
+
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("movie-poster-card.fxml"));
+                Node cardNode = loader.load();
+
+                MoviePosterController cardController = loader.getController();
+
+                cardController.setData(image);
+
+                TrendingPosterRow.getChildren().add(cardNode);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Couldn't load Trending posters");
+        }
+    }
+
+}
--- src/main/java/org/openjfx/sfaxbest/MoviePosterCardController.java
+++
@@ -1,4 +0,0 @@
-package org.openjfx.sfaxbest;
-
-public class MoviePosterCardController {
-}
