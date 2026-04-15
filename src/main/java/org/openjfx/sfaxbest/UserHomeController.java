1f7626bbea921114f115909bc44ef47144692dd5
jo bangalo <jawhermonastiri10@gmail.com>
2026-04-15 17:36:53 +0100

    sfaxBest 0.4 user avatar change button functional

--- src/main/java/org/openjfx/sfaxbest/UserHomeController.java
+++ src/main/java/org/openjfx/sfaxbest/UserHomeController.java
@@ -1,12 +1,64 @@
 package org.openjfx.sfaxbest;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.TranslateTransition;
+import entities.User;
+
 import javafx.fxml.FXML;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.util.Duration;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javafx.stage.FileChooser;
+import java.io.File;
+import java.nio.file.StandardCopyOption;
 
 public class UserHomeController {
 
+    User user = MainViewController.instance.getCurrentUser();
+
+    @FXML ImageView avatarImageView;
+    @FXML Circle avatarClip;
+    @FXML Label usernameLabel;
+    @FXML Label emailLabel;
+
+    public void initialize() {
+        setData();
+    }
+    @FXML
+    public void changeAvatar() throws IOException {
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg"));
+        fileChooser.setTitle("Choose an Avatar");
+
+        File selectedFile = fileChooser.showOpenDialog(avatarClip.getScene().getWindow());
+
+
+        if (selectedFile != null) {
+            try {
+                String fileName = selectedFile.getName();
+                File imagesFolder = new File("src/main/resources/Images");
+                Path destinationPath = Path.of(imagesFolder.getAbsolutePath(), fileName);
+                Files.copy(selectedFile.toPath(), destinationPath, StandardCopyOption.REPLACE_EXISTING);
+                setAvatar(new Image(destinationPath.toUri().toString()));
+                user.setAvatarUrl("/Images" + fileName);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setData() {
+        setAvatar(new Image(getClass().getResource(user.getAvatarUrl()).toExternalForm()));
+        usernameLabel.setText(user.getUsername());
+        emailLabel.setText(user.getEmail());
+    }
+    private void setAvatar(Image image) {
+        avatarClip.setFill(new ImagePattern(image));
+    }
 }
